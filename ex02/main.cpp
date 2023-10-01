be71cae82c232aeea65b0520e58157ca034e77ef
Khaled Alshaer <kalshaer@lab1r2s10.42abudhabi.ae>
2023-10-01 12:06:05 +0400

    final

---
+++ ex02/main.cpp
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kalshaer <kalshaer@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/01 12:03:05 by kalshaer          #+#    #+#             */
+/*   Updated: 2023/10/01 12:04:53 by kalshaer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+Base * generate(void)
+{
+	int i = rand() % 3;
+	if (i == 0)
+		return (new A());
+	else if (i == 1)
+		return (new B());
+	else
+		return (new C());
+}
+
+void identify_from_pointer(Base * p)
+{
+	if (dynamic_cast<A*>(p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C*>(p))
+		std::cout << "C" << std::endl;
+}
+
+void identify_from_reference(Base & p)
+{
+	if (dynamic_cast<A*>(&p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B*>(&p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C*>(&p))
+		std::cout << "C" << std::endl;
+}
+
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << "Test " << i << std::endl;
+		Base *base = generate();
+		identify_from_pointer(base);
+		identify_from_reference(*base);
+		delete base;
+	}
+}
+
